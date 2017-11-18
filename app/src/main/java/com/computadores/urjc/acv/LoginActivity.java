2ea98998ab85ccc2f858442a4ad9bb0feedf1cf9
Jesús <was121995@gmail.com>
2017-11-18 13:29:36 +0100

    improve DialogLogin

--- app/src/main/java/com/computadores/urjc/acv/LoginActivity.java
+++ app/src/main/java/com/computadores/urjc/acv/LoginActivity.java
@@ -15,34 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         Button button_inciar_sesion=(Button) findViewById(R.id.button_inicio_sesion);
         button_inciar_sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* Intent intent=new Intent(LoginActivity.this,MenuActivity.class);
-                startActivity(intent);
-                finish();
-                */
-                AlertDialog.Builder builder;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    builder = new AlertDialog.Builder(getApplicationContext(), android.R.style.Theme_Material_Dialog_Alert);
-                } else {
-                    builder = new AlertDialog.Builder(getApplicationContext());
-                }
-                builder.setTitle("Delete entry")
-                        .setMessage("Are you sure you want to delete this entry?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // continue with delete
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        })
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                AlertDialog.Builder builder= new AlertDialog.Builder(LoginActivity.this);
+                View view=getLayoutInflater().inflate(R.layout.dialog_inicio_sesion,null);
+                builder.setView(view);
+                AlertDialog dialog=builder.create();
+                dialog.show();
             }
         });
     }
