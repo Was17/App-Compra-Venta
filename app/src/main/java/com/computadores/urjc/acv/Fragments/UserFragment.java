7dda4f3c6968b73fb997edd603b913e7ef0e182f
David Garcia Herrero <david160296@gmail.com>
2018-01-10 19:40:25 +0100

    email

--- app/src/main/java/com/computadores/urjc/acv/Fragments/UserFragment.java
+++ app/src/main/java/com/computadores/urjc/acv/Fragments/UserFragment.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,6 +23,15 @@ import com.computadores.urjc.acv.Database.UserDatabase;
 import com.computadores.urjc.acv.R;
 
 import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,7 +75,7 @@ public class UserFragment extends Fragment {
             nombre=(TextView) itemView.findViewById(R.id.card_title);
         }
     }
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
         // Set numbers of List in RecyclerView.
         ArrayList<User> objetos;
         UserDatabase userDatabase;
@@ -89,9 +100,51 @@ public class UserFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
+
             User user= objetos.get(position);
             holder.nombre.setText(user.getNombre());
             holder.mCardViewTop.setCardBackgroundColor(Color.GRAY);
+
+
+
+
+              final String mensaje="Esto es una prueba";
+
+              final String correo="CompraVentaURJC@gmail.com";
+              final String contraseña = "compraventaldm18";
+            holder.mCardViewTop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    Properties properties= new Properties();
+                    properties.put("mail.smtp.host","smtp.googlemail.com");
+                    properties.put("mail.smtp.socketFactory.port","465");
+                    properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+                    properties.put("mail.smtp.auth","true");
+                    properties.put("mail.smtp.port","465");
+
+                    try {
+                    Session   session = Session.getDefaultInstance(properties, new Authenticator() {
+                            @Override
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return  new PasswordAuthentication(correo,contraseña);
+                            }
+                        });
+                        if(session!=null){
+                            Message message= new MimeMessage(session);
+                            message.setFrom(new InternetAddress(correo));
+                            message.setSubject(("asunto urjc"));
+                            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(""));
+                            message.setContent(mensaje,"text/html; charset=utf-8");
+                            Transport.send(message);
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
         }
 
         @Override
