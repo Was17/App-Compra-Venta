4fce89a077b44fd1cb96f345b9f29454b7364998
Jesús <was121995@gmail.com>
2018-01-14 00:07:16 +0100

    Database

--- app/src/main/java/com/computadores/urjc/acv/Database/Database.java
+++ app/src/main/java/com/computadores/urjc/acv/Database/Database.java
@@ -1,4 +1,4 @@
-package com.computadores.urjc.acv.Database;
+package com.example.was12.myapplication.acv.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -26,7 +26,11 @@ public class Database extends SQLiteOpenHelper {
     // Table Names
     private static final String TABLE_USERS = "users";
     private static final String TABLE_ARTICULOS = "articulos";
-    private static final String TABLE_CHAT = "chat";
+    private static final String TABLE_INTERESA = "interesa";
+
+    static final String ID_INTERESA="_id";
+    static final String INTERESA_USER="user";
+    static final String INTERESA_ARTICULO="articulo";
 
     static final String ID="_id";
     static final String NAME="name";
@@ -47,7 +51,12 @@ public class Database extends SQLiteOpenHelper {
             + " TEXT NOT NULL," + PRECIO + " TEXT NOT NULL," + DESCRIPCION
             + " TEXT NOT NULL,"+PHOTO_ARTICULO + " BOLD NOT NULL," + ARTICULO_ID_USER
             + " TEXT NOT NULL" + ")";
-   private static final String CREATE_TABLE_USER = "CREATE TABLE "
+
+    private static final String CREATE_TABLE_INTERESA = "CREATE TABLE "
+            + TABLE_INTERESA + "(" + ID_INTERESA + " INTEGER PRIMARY KEY AUTOINCREMENT," + INTERESA_USER
+            + " TEXT NOT NULL," + INTERESA_ARTICULO + " TEXT NOT NULL" + ")";
+
+    private static final String CREATE_TABLE_USER = "CREATE TABLE "
             + TABLE_USERS + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + NAME
             + " TEXT NOT NULL," + PASSWORD + " TEXT NOT NULL," + EMAIL
             + " TEXT NOT NULL,"+PHOTO + ")";
@@ -62,6 +71,7 @@ public class Database extends SQLiteOpenHelper {
         // creating required tables
         db.execSQL(CREATE_TABLE_ARTICULO);
         db.execSQL(CREATE_TABLE_USER);
+        db.execSQL(CREATE_TABLE_INTERESA);
     }
 
     @Override
@@ -69,7 +79,7 @@ public class Database extends SQLiteOpenHelper {
         // on upgrade drop older tables
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARTICULOS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CHAT);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_INTERESA);
 
         // create new tables
         onCreate(db);
@@ -94,14 +104,14 @@ public class Database extends SQLiteOpenHelper {
     }
     public Cursor getUserByName(String name) {
         Cursor cursor=null;
-    try{
-        cursor = this.sqLiteDatabase.query(true,TABLE_USERS,new String[]{ID,NAME,EMAIL,PASSWORD,PHOTO}, NAME+ "=?", new String[] { name }, null, null, null,null );
-        if(cursor!=null){
-            cursor.moveToFirst();
-        }
-    }catch (Exception e){
+        try{
+            cursor = this.sqLiteDatabase.query(true,TABLE_USERS,new String[]{ID,NAME,EMAIL,PASSWORD,PHOTO}, NAME+ "=?", new String[] { name }, null, null, null,null );
+            if(cursor!=null){
+                cursor.moveToFirst();
+            }
+        }catch (Exception e){
 
-    }
+        }
 
         return cursor;}
     public Cursor getUserByEmail(String email) {
@@ -144,45 +154,99 @@ public class Database extends SQLiteOpenHelper {
 
     //ARTICULOS   static final String ID_ARTICULOS ="_id";
 
-    public long insertArticulo(String name,String email,String password,String foto){
+    public long insertInteres(String idUser,String idArticulos){
         ContentValues contentValues=new ContentValues();
-        contentValues.put(NAME_ARTICULOS,name);
-        contentValues.put(PHOTO_ARTICULO,foto);
-        contentValues.put(PRECIO,email);
-        contentValues.put(DESCRIPCION,password);
-        contentValues.put(ARTICULO_ID_USER,password);
-        return sqLiteDatabase.insert(TABLE_ARTICULOS,null,contentValues);
+        contentValues.put(INTERESA_USER,idUser);
+        contentValues.put(INTERESA_ARTICULO,idArticulos);
+        return sqLiteDatabase.insert(TABLE_INTERESA,null,contentValues);
     }
-    public boolean delete(long id){
-        return sqLiteDatabase.delete(TABLE_ARTICULOS, ID_ARTICULOS +"="+id,null)>0;
+    public boolean delete_interes(long id){
+        return sqLiteDatabase.delete(TABLE_INTERESA, ID_INTERESA +"="+id,null)>0;
     }
-    public Cursor getAllArticulos(){
-        return sqLiteDatabase.query(TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,ARTICULO_ID_USER},null,null,null,null,null);
-    }
-    public Cursor getChatMessages(String name)throws SQLException{
-        Cursor cursor;
+    public Cursor getInterestByUser(String id) {
+        Cursor cursor = null;
+        try {
+            cursor = this.sqLiteDatabase.query(true, TABLE_INTERESA, new String[]{ID_INTERESA, INTERESA_USER, INTERESA_ARTICULO}, INTERESA_USER + "=?", new String[]{id}, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+            }
+        } catch (Exception e) {
 
-        cursor = this.sqLiteDatabase.query(true,TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,ARTICULO_ID_USER}, "(" + NAME_ARTICULOS + " like '%" + name + "%' OR " + NAME_ARTICULOS + " like '%" + "j" + "%')", null, null, null, null,null );
-        if(cursor!=null){
-            cursor.moveToFirst();
         }
-        return cursor;}
-    public Cursor getArticulo(long rowID)throws SQLException{
+        return cursor;
+    }
+    public Cursor getAllInteres(){
+        return sqLiteDatabase.query(TABLE_INTERESA,new String[]{ID_INTERESA, INTERESA_USER,INTERESA_ARTICULO},null,null,null,null,null);
+    }
 
-        Cursor cursor=sqLiteDatabase.query(true,TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,ARTICULO_ID_USER}, ID_ARTICULOS +"="+rowID,null,null,null,null,null);
+    public Cursor getInteres(long rowID)throws SQLException{
+
+        Cursor cursor=sqLiteDatabase.query(true,TABLE_INTERESA,new String[]{ID_INTERESA, INTERESA_USER,INTERESA_ARTICULO}, ID_INTERESA +"="+rowID,null,null,null,null,null);
         if(cursor!=null){
             cursor.moveToFirst();
         }
         return cursor;
 
     }
-    public boolean updateArticulo(long rowId,String name,String email,String password,String foto){
+    public boolean updateInteres(long rowId,String idUser,String idArticulo){
+        ContentValues args=new ContentValues();
+        args.put(INTERESA_USER,idUser);
+        args.put(INTERESA_ARTICULO,idArticulo);
+        return sqLiteDatabase.update(TABLE_INTERESA,args, ID_INTERESA +"="+rowId,null)>0;
+    }
+    //ARTICULOS   static final String ID_ARTICULOS ="_id";
+
+    public long insertArticulo(String name,String email,String password,String foto,String id){
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(NAME_ARTICULOS,name);
+        contentValues.put(PHOTO_ARTICULO,foto);
+        contentValues.put(PRECIO,email);
+        contentValues.put(DESCRIPCION,password);
+        contentValues.put(ARTICULO_ID_USER,id);
+        return sqLiteDatabase.insert(TABLE_ARTICULOS,null,contentValues);
+    }
+    public boolean delete(long id){
+        return sqLiteDatabase.delete(TABLE_ARTICULOS, ID_ARTICULOS +"="+id,null)>0;
+    }
+    public Cursor getAllArticulos(){
+        return sqLiteDatabase.query(TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,PHOTO_ARTICULO,ARTICULO_ID_USER},null,null,null,null,null);
+    }
+    public Cursor getChatMessages(String name)throws SQLException{
+        Cursor cursor;
+
+        cursor = this.sqLiteDatabase.query(true,TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,PHOTO_ARTICULO,ARTICULO_ID_USER}, "(" + NAME_ARTICULOS + " like '%" + name + "%' OR " + NAME_ARTICULOS + " like '%" + "j" + "%')", null, null, null, null,null );
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        return cursor;}
+    public Cursor getArticulo(long rowID)throws SQLException{
+
+        Cursor cursor=sqLiteDatabase.query(true,TABLE_ARTICULOS,new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,PHOTO_ARTICULO,ARTICULO_ID_USER}, ID_ARTICULOS +"="+rowID,null,null,null,null,null);
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+
+    }
+    public Cursor getArticuloByUser(String id) {
+        Cursor cursor = null;
+        try {
+            cursor = this.sqLiteDatabase.query(true, TABLE_ARTICULOS, new String[]{ID_ARTICULOS, NAME_ARTICULOS, PRECIO,DESCRIPCION,PHOTO_ARTICULO,ARTICULO_ID_USER}, ARTICULO_ID_USER + "=?", new String[]{id}, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+            }
+        } catch (Exception e) {
+
+        }
+        return cursor;
+    }
+    public boolean updateArticulo(long rowId,String name,String email,String password,String foto,String id){
         ContentValues args=new ContentValues();
         args.put(NAME_ARTICULOS,name);
         args.put(PHOTO_ARTICULO,foto);
         args.put(PRECIO,email);
         args.put(DESCRIPCION,password);
-        args.put(ARTICULO_ID_USER,password);
+        args.put(ARTICULO_ID_USER,id);
         return sqLiteDatabase.update(TABLE_ARTICULOS,args, ID_ARTICULOS +"="+rowId,null)>0;
     }
     // closing database
